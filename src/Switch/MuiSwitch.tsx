14eca0568ce374f89c82eca6036c3e77cc2369c1
Kerem Akkus <keremiakkus@gmail.com>
2023-04-14 18:15:58 +0300

    React Material UI Switch

---
+++ src/Switch/MuiSwitch.tsx
@@ -0,0 +1,14 @@
+import { Switch, Box, FormControlLabel} from "@mui/material"
+import { useState } from "react"
+export const MuiSwitch = () => {
+    const [darkMode, setDarkMode] = useState<boolean>(false)
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDarkMode(event.target.checked)
+    }
+  return (
+    <Box>
+        <FormControlLabel label='Dark mode' control={<Switch checked={darkMode} onChange={handleChange}/>}/>
+    </Box>
+  )
+}
+""
