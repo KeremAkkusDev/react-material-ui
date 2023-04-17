e95186f2c7a60dcf05ee12d71899e8b142a56be2
Kerem Akkus <keremiakkus@gmail.com>
2023-04-17 09:10:06 +0300

    React Material UI Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import { MuiGrid } from "./Grid/MuiGrid";
+import { MuiCard } from "./Card/MuiCard";
 
 function App() {
   return <div className="App">
-    <MuiGrid/>
+    <MuiCard/>
   </div>;
 }
 
