18f0c94aa690374574d95182ece03e357405c30b
KeremAkkusGitHub <122130469+KeremAkkusDev@users.noreply.github.com>
2023-04-24 09:17:30 +0300

    React Material UI Date Range Picker

--- src/Picker/MuiPicker.tsx
+++ src/Picker/MuiPicker.tsx
@@ -1,40 +1,59 @@
-import { Stack } from "@mui/material";
-import { DatePicker, DateTimePicker, TimePicker } from "@mui/x-date-pickers";
+import { Stack, Box } from "@mui/material";
+import {
+  DatePicker,
+  DateTimePicker,
+  TimePicker,
+  DateRangePicker,
+  DateRange,
+} from "@mui/x-date-pickers-pro";
 import { useState } from "react";
 
 export const MuiPicker = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [selectedTime, setSelectedTime] = useState<Date | null>(null);
   const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null);
-
+  const [value, setValue] = useState<DateRange<Date>>([null, null]);
   console.log({
     selectedDate,
     selectedTime: selectedTime && selectedTime.toLocaleTimeString(),
     selectedDateTime,
+    value,
   });
   return (
-    <Stack spacing={4} sx={{ width: "250px" }}>
-      <DatePicker
-        label="Date Picker"
-        value={selectedDate}
-        onChange={(newValue) => {
-          setSelectedDate(newValue);
-        }}
-      />
-      <TimePicker
-        label="Time Picker"
-        value={selectedTime}
-        onChange={(newValue) => {
-          setSelectedTime(newValue);
-        }}
-      />
-      <DateTimePicker
-        label="Date Time Picker"
-        value={selectedDateTime}
-        onChange={(newValue) => {
-          setSelectedDateTime(newValue);
-        }}
-      />
-    </Stack>
+    <Box>
+      <Stack spacing={4} sx={{ width: "250px" }}>
+        <DatePicker
+          label="Date Picker"
+          value={selectedDate}
+          onChange={(newValue) => {
+            setSelectedDate(newValue);
+          }}
+        />
+        <TimePicker
+          label="Time Picker"
+          value={selectedTime}
+          onChange={(newValue) => {
+            setSelectedTime(newValue);
+          }}
+        />
+        <DateTimePicker
+          label="Date Time Picker"
+          value={selectedDateTime}
+          onChange={(newValue) => {
+            setSelectedDateTime(newValue);
+          }}
+        />
+      </Stack>
+      <br />
+      <Stack>
+        <DateRangePicker
+          label="Date Range Picker"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Stack>
+    </Box>
   );
 };
