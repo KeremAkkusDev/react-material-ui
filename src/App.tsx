172f732e27c04349ee32e9916d70805c22472a64
KeremAkkusGitHub <122130469+KeremAkkusDev@users.noreply.github.com>
2023-04-24 13:20:10 +0300

    React Material UI Customizing Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import "./App.css";
-import { MuiResponsiveness } from "./Responsiveness/MuiResponsiveness";
+import { createTheme, colors, ThemeProvider } from "@mui/material";
+import { MuiCustomizingTheme } from "./CustomizingTheme/MuiCustomizingTheme";
+
+const theme = createTheme({
+  status: {
+    danger: colors.orange[500],
+  },
+  palette: {
+    primary: {
+      main: colors.blue[500],
+    },
+    secondary: {
+      main: colors.red[500],
+    },
+    neutral: {
+      main: colors.grey[500],
+      darker: colors.grey[700],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <MuiResponsiveness />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MuiCustomizingTheme />
+      </div>
+    </ThemeProvider>
   );
 }
 
